449e5a6aaafbf7d6a1df9c77ecf4543f44816bee
Sasha Kaminska <sasha249133@ukr.net>
2017-10-30 00:08:00 +0200

    created class Node

--- src/com/company/collections/Node.java
+++ src/com/company/collections/Node.java
@@ -1,37 +1,42 @@
 package com.company.collections;
 
-public class MyNode<T> {
-    private T element;
-    private MyNode<T> prev;
-    private MyNode<T> next;
+public class Node<T> {
+    private T object;
+    private Node<T> prev;
+    private Node<T> next;
 
-    public MyNode(T element, MyNode<T> prev, MyNode<T> next) {
-        this.element = element;
+    public Node() {
+        this(null, null, null);
+    }
+
+    public Node(T element, Node<T> prev, Node<T> next) {
+        this.object = element;
         this.prev = prev;
         this.next = next;
     }
 
-    public T getElement() {
-        return element;
+    public T getObject() {
+        return object;
     }
 
-    public void setElement(T element) {
-        this.element = element;
+    public void setObject(T object) {
+        this.object = object;
     }
 
-    public MyNode<T> getPrev() {
+    public Node<T> getPrev() {
         return prev;
     }
 
-    public void setPrev(MyNode<T> prev) {
+    public void setPrev(Node<T> prev) {
         this.prev = prev;
     }
 
-    public MyNode<T> getNext() {
+    public Node<T> getNext() {
         return next;
     }
 
-    public void setNext(MyNode<T> next) {
+    public void setNext(Node<T> next) {
         this.next = next;
     }
+
 }
